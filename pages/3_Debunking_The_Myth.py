8de41ac7428252bc3a16f02dec79860666bdb9fc
hrhendersonboyer-admin <hrhendersonboyer@crimson.ua.edu>
2024-06-22 14:38:59 -0700

    Adding myth debunking page to site

--- pages/3_Debunking_The_Myth.py
+++ pages/3_Debunking_The_Myth.py
@@ -1,3 +1,26 @@
 import streamlit as st
 
-st.write("# The real deal about the Ivy(+) dream (and why it's so problematic)")
+st.write("# Facts and myths about attending highly selective schools :books::books:")
+st.markdown(
+"""
+## Myth: Going to a highly selective school is a golden ticket to success.
+Fact: No matter where you go to college, you still have to both excel academically and do cool things outside of class (like research, cool clubs, side projects, or something else unique!) to stand out when you apply for jobs and grad programs. Sometimes going to a highly-selective school renowned for its academics can help you get a deeper mastery of the material in the classroom, but it isn't always the best for getting diverse outside-of-class experiences. For example, clubs at Princeton and other highly-selective schools are often competitive. Imagine a 10-percent acceptance rate...just to join an improv club! In contrast, at lots of state universities (like Alabama!), you can join the improv club or do meaningful research or design software for moon-mining robots or (insert awesomeness here) as soon as you start your freshman year.
+
+## Myth: You'll only get hired at a top company if you go to a highly selective school.
+Fact: People believe this because it was often the case in the past (pre-2000) that companies would use school name as a filter for talent. Most companies have now realized that that's a super dumb idea and they're losing out on a super talented and diverse workforce. For example, if you're looking to get into tech, FAANG companies don't care about school name--they care what cool stuff you've done (see the previous point about extracurriculars and side projects). That isn't to say that there aren't still a few companies out there that care about school name (the McKinseys of the world), but ask yourself: Do you really want to work at a company that has clients who only want to work with people with a certain "pedigree"? Pedigrees are for dogs, not people. No thanks.
+
+## Myth: "Ok, that's great, but I want to make a lot of money after college. Isn't the big money at places that care about school name?"
+Fact: Nope! Luckily, snooty consulting firms do not have a monopoly on the big money. In fact, this 2011 study (https://www.nber.org/papers/w17159) shows that among people who get admitted to Ivy(+) universities, people who actually attended tended to do about the same in the money department as those who didn't.
+
+## Myth: "You make some good points, but I want to be a CEO someday. Don't I need to go to an Ivy(+) school for that?
+Fact: Go get it boss!! Also, nope! According to Forbes (https://www.forbes.com/sites/kimberlywhitler/2019/09/07/a-new-study-on-fortune-100-ceos-what-undergraduate-institutions-did-they-attend/), 90 percent of Fortune 100 CEOs didn't go to Ivies and most went to state schools. How cool is that?
+
+## Myth: "But I want a Wall Street job, and they definitely target highly selective schools. If I can't afford to go to one, I'm out of luck."
+Fact: Also nope! I'll admit that lots of Wall Street firms still have "target schools," which I personally find super gross. But if you're in a financial situation where you don't get enough aid to go to a highly selective school, there is a better way to make it to Wall Street than taking on hundreds of thousands in debt. Get some work experience under your belt at a smaller firm, get an MBA at a top school, and then apply to your target companies. Once you have the MBA it will matter waaaay more than where you went to undergrad, as will your work experience. (Source: https://www.investopedia.com/financial-edge/1110/how-to-get-a-job-on-wall-street.aspx)  
+Life hack: The grad school trick works most places where school name matters more than it should. If you really want to work somewhere that values a "prestigious" school but you don't get enough aid to attend, go to a cheaper school for undergrad and save money for a top grad program. This is also what a lot of med school students do, because med school can be EXPENSIVE!
+
+## Myth: "Wait, are you dissing Ivies here?"
+Fact: No, of course not! Ivies are great schools with lots of resources and opportunities. But they're not the only schools with resources and opportunities. If you're a student who's excited about the idea of going to an Ivy, that's awesome! But if you're a student who's feeling pressured to go to an Ivy because you think it's the only way to be successful, that's not awesome. You should go to the school that's the best fit for you, not the school that's the best fit for someone else's idea of what success looks like.
+
+"""
+)
